f20b8022130cf7b87096467ea10bdd0726b445e9
soumyasinha514 <soumyasinha514@gmail.com>
2024-07-25 20:17:45 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 
-import 'package:bma_cars/screens/profile.dart';
-import 'package:bma_cars/screens/profile_details.dart';
-import 'package:bma_cars/screens/quotation_screen.dart';
+
 import 'package:bma_cars/screens/splash.dart';
-import 'package:bma_cars/screens/notification.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
        
         useMaterial3: true,
       ),
-      home:  ProfileDetailScreen()
+      home:  const SplashScreen()
     );
   }
 }
